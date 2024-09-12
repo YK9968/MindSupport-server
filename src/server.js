be1adea51742f4c46385af47cd80996ce6bec9c1
yurii <robka4an@gmail.com>
2024-09-12 17:10:45 +0300

    start server

---
+++ src/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import cors from 'cors';
+import { env } from './utils/env.js';
+
+const PORT = Number(env('PORT', '3000'));
+export default function setupServer(second) {
+  const app = express();
+  app.use(express.json());
+  app.use(cors());
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
